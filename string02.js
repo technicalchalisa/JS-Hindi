4de3dbba4bc52c0e848bfb0309e639303e8d99a8
Arshad Alam <nutrients816@gmail.com>
2023-11-16 09:39:50 +0530

    Math

--- string02.js
+++ string02.js
@@ -8,9 +8,9 @@
    
 //    demoFun(10,25)
 
-const score = 500
+// const score = 500
 
-const balance = new Number(500)
+// const balance = new Number(500)
 
 // console.log(score);
 // const strBalance = balance.toString()
@@ -23,7 +23,28 @@ const balance = new Number(500)
 // console.log(deciNum.toPrecision(3));
 
 
-const localString = 10000000
-const anotherNumber = localString.toLocaleString('en-IN')
+// const localString = 10000000
+// const anotherNumber = localString.toLocaleString('en-IN')
 
-console.log( anotherNumber);
+// console.log( anotherNumber);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++Math++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.round(7.383));
+// console.log(Math.round(7.83));
+// console.log(Math.abs(-5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(6.2));
+// console.log(Math.min(6.2, 3,53,64,90));
+// console.log(Math.max(6.2, 3,53,64,90));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// noted point
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
