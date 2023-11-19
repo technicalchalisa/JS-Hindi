3711e0a78b4dfc530ebdd046dccf63f9dcd457b8
Arshad Alam <nutrients816@gmail.com>
2023-11-19 18:56:25 +0530

    switch and truthy

---
+++ switch10.js
@@ -0,0 +1,23 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
---
+++ truthy11.js
@@ -0,0 +1,48 @@
+const userEmail = "nagama.ai"
+
+// if (userEmail) {
+    //     console.log("Got user email");
+    // }else{
+        //     console.log("Don't  have user email");
+        // }
+        
+        // falsy values
+        // false, 0, -0, BigInt 0n, "", null, undefined, NaN
+        
+        //truthy values
+        // "0", 'false', " ", [], {}, function(){}, 
+
+        const checkForArray = []
+        
+        if (checkForArray.length === 0) {
+            // console.log("Array is empty");
+        }
+
+
+        const checkForObject = {}
+
+        if (Object.keys(checkForObject).length === 0) {
+            // console.log("Object is empty");
+        }
+
+        // Nullish Coalescing Operator (??): null undefined
+
+        let val1;
+    //    val1 = 5 ?? 10
+    //    val1 = null ?? 10
+    //   val1 = undefined ?? 15
+        val1 = null ?? 10 ?? 30
+
+
+        // console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >=  80 ? console.log("less then 80") : console.log("more then 80");
+
+
+
+
