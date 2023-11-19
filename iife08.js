5543cb35b2d1590bbb616dab19c1eedcae9cefd8
Arshad Alam <nutrients816@gmail.com>
2023-11-19 13:08:19 +0530

    IIFE

---
+++ iife08.js
@@ -0,0 +1,11 @@
+//Immediately Invoked Function Expressions (IIFE)
+(function eSolutionHub() {
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+
+(   (name) => {
+    //unnamed IIFE
+    console.log(`DB CONNECTED TWO ${name}`);
+} ) ("nagama")
